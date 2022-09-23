22fa8c10003cc169db28eed3423aad0946cf12b3
Eitasato <miyadeek@gmail.com>
2022-09-23 19:19:08 +0200

    プロフィールテキスト更新

--- page-profile.php
+++ page-profile.php
@@ -6,38 +6,46 @@
   </div>
     <article class="profile">
       <h3 class="prof-header">栗谷さと子</h3>
-      <p>1988年福井県生まれ。神奈川県出身。
-2007年、長野県松本市に移住し、信州大学理学部で生物科学を専攻。
+      <p>11988年福井県生まれ。主に神奈川県で育つ。
+2007年、長野県松本市に移住。山に惹かれて進学した信州大学理学部、信州大学大学院で生物科学を専攻。動植物の観察スケッチが得意だった。
 
-動植物の観察スケッチが得意だった。
-卒業後の現在も松本に残り、カンデラゲストハウスを営みつつ、イラストレーターとしても活動。
+卒業後は松本に残りカンデラゲストハウスや浅間温泉下宿 篶竹荘 / 第2ペンギン荘を運営しながら、イラストレーターとしても経験を重ね、現在は画業を活動の中心としている。
 
-風景、似顔絵などをあたたかなタッチで描き、雑誌の連載の他、個人様からの制作依頼（結婚式ウェルカムボード、お店や思い出の風景画など）も引き受けている。
+雑誌の連載や商品パッケージのイラストの他、個人様からのさまざまな制作依頼も引き受けており、依頼人様の想いをくみ取る事を大切にしながら仕事をしている。
 
-趣味は旅、登山、サイクリング。
-旅先でのスケッチおよび旅日記の展覧会は国内外の各地で行っている。</p>
+趣味は旅、登山、サイクリング。旅先や日常生活での事をイラスト日記に描くのはライフワークの一つで、旅先でのスケッチおよび旅日記の展覧会は国内外の各地で行っている。
+
+2022年、ニュージーランドへ移住。小さな奇跡の積み重ねが導く予想外な人生の展開を楽しもうと思っている。
+</p>
     <h3 class="prof-header">KURIYA SATOKO ( KURI )</h3>
-    <p>I was born in 1988.
-I have lived in Matsumoto, Nagano pref., Japan since 2007.
-My major in university was biology, so that I am good at observe creatures carefully and sketch  it.
-After finished master degree program in university, I have been working at Candela Guest House and doing illustration work.
-​My painting is mainly portrait and landscape painting with warm touch.
-I paint for magazines and personal clients.
-​My hobby is trekking, cycling and traveling.
-I had exhibitions of my travel sketch in many places in Japan and Taiwan.</p>
+    <p>I was born in Japan in 1988.
+
+My major in university was biology, so I am good at observing creatures carefully and sketching them.
+
+After finishing my master degree program in university in 2013, I started my career as an artist while working at Candela Guest House.
+
+​My works are mainly portrait and landscape painting with a warm touch of watercolors, but also I do printmaking, mural painting and so on.
+
+I love trekking, cycling and traveling. Drawing visual journals to capture things about my daily life and travel is one of my life's work and I like to share it with other people. I had exhibitions of my travel sketches in many places in Japan and Taiwan.
+
+I moved from Japan to New Zealand in 2022. I want to enjoy the unexpected life led by heaps of little miracles.
+</p>
 
         <h4 class="prof-header">連載</h4>
 
         <ul>
-          <li>「M’s Life」表紙絵（信濃毎日新聞松本専売所）</li>
-          <li>「MOTOツーリング」旅する絵描きの想筆紀行（内外出版社）
-          </li>
+			<li>「M’s Life（信濃毎日新聞松本専売所）」表紙絵連載（2016～2021年）</li>
+			<li>「MOTOツーリング（内外出版社）」旅する絵描きの想筆紀行連載（2018年～）</li>
+			<li>風景画</li>
+			<li>似顔絵</li>
+			<li>名刺作成</li>
+			<li>消しゴムはんこ制作</li>
+			<li>パッケージデザイン</li>
         </ul>
 
         <h4 class="prof-header">ご依頼について</h4>
-        <p>お仕事についてのご依頼・ご相談をいただける場合は下記アドレス宛にEメールをお送り下さい。
-内容を確認後、折り返しご返事させていただきます。どうぞよろしくお願い申し上げます。
-恐れ入りますが個人の方からのご依頼やご相談は基本的にお受けできません、ご了承下さい。</p>
+        <p>お仕事についてのご依頼・ご相談をいただける場合はContactフォームよりEメールをお送り下さい。
+内容を確認後、折り返しお返事させていただきます。どうぞよろしくお願い申し上げます。</p>
         </article>
   </main>
 <?php get_footer(); ?>
